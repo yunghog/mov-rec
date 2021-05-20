fb41de203eebc4cc39e350e5c16968f1c5a4a5f8
Samartha <samarthahm@gmail.com>
2021-05-20 09:45:29 +0530

    movie details page

---
+++ src/components/cards-array-2.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/css/style.css'
+
+class CardsArray2 extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+    }
+  }
+  render(){
+    const list = this.props.list;
+    return(
+      <Container className="cards-array-con">
+        <Row>
+          <Col>
+            <h4>{this.props.title}</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="movie-thumb-con">
+              {list && list.map(item=>
+                <a href={"#/people/"+item.id}>
+                  <div className="movie-cast" key={item.id}>
+                    <img src={"https://image.tmdb.org/t/p/w500"+item.profile_path}/>
+                    <p>{item.name}</p>
+                </div>
+                </a>
+              )}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+export default CardsArray2
--- src/pages/movie-details.js
+++ src/pages/movie-details.js
@@ -3,13 +3,17 @@ import {Container, Col, Row} from 'react-bootstrap'
 import Axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/style.css'
+import CardsArray from  '../components/cards-array'
+import CardsArray2 from  '../components/cards-array-2'
 
 class MovieDetails extends React.Component{
   constructor(props){
     super(props)
     this.state={
       movieId: '',
-      thisMovie: ''
+      thisMovie: '',
+      similarMovies:  [],
+      cast: ''
     }
   }
   componentDidMount(){
@@ -17,19 +21,43 @@ class MovieDetails extends React.Component{
       movieId:this.props.match.params.movieId
     })
     this.getMovieDetails(this.props.match.params.movieId);
+    this.getSimilarMovies(this.props.match.params.movieId);
+    this.getCredits(this.props.match.params.movieId);
   }
   getMovieDetails = (id) =>{
     let url = "https://api.themoviedb.org/3/movie/"+id+"?api_key=e0027d57cef3fdd5caa5a8e762c30c62&language=en-US".replace(" ","");
-    console.log(url);
     Axios.get(url).then(res=>{
-      console.log(res.data);
       this.setState({
-        thisMovie: res.data
+        thisMovie: res.data,
       })
     })
   }
+  getCredits = (id) =>{
+    let url = "https://api.themoviedb.org/3/movie/"+id+"/credits?api_key=e0027d57cef3fdd5caa5a8e762c30c62&language=en-US".replace(" ","");
+    Axios.get(url).then(res=>{
+      this.setState({
+        cast: res.data
+      })
+    })
+  }
+  getSimilarMovies = (id) =>{
+    let url = "https://api.themoviedb.org/3/movie/"+id+"/similar?api_key=e0027d57cef3fdd5caa5a8e762c30c62&language=en-US&page=1".replace(" ","");
+    Axios.get(url).then(res=>{
+      this.setState({
+        similarMovies: res.data.results,
+      })
+    })
+  }
+  componentDidUpdate(){
+    if(this.state.movieId != this.props.match.params.movieId){
+      window.location.reload();
+    }
+  }
   render(){
     const movie = this.state.thisMovie;
+    const similar = this.state.similarMovies;
+    const cast = this.state.cast.cast;
+    console.log(cast);
     return(
       <div>
         <Container fluid>
@@ -49,17 +77,36 @@ class MovieDetails extends React.Component{
         <br/>
         <Container>
           <Row>
+            <Col md={9}>
+              <div className="movie-details">
+                <h1>{movie.title}</h1>
+                {movie.tagline && <h5>{movie.tagline}</h5>}
+                {movie.genres && movie.genres.map(i=>
+                  <a className="genre"  key={i.id}>{i.name}</a>
+                )}
+                <p>{movie.overview}</p>
+                  <div className="highlight1">
+                    <li>Release : {movie.release_date}</li>
+                    <li>TMDB Rating : {movie.vote_average}</li>
+                    {movie.production_companies && <li>Production : { movie.production_companies.map(comp=>
+                        <li>{comp.name},</li>
+                      )}</li>}
+                    <li style={{textTransform:"uppercase"}}>Language : {movie.original_language}</li>
+                    <li>Budget : ${movie.budget}</li>
+                    
+                  </div>
+              </div>
+            </Col>
             <Col md={3}>
               <div className="movie-poster">
                 <img src={"https://image.tmdb.org/t/p/original"+movie.poster_path}/>
               </div>
             </Col>
-            <Col md={9}>
-              <div className="movie-details">
-                
-              </div>
-            </Col>
           </Row>
+          <br/>
+          <CardsArray2 list={cast} title="Movie Cast"/>
+          <br/>
+          {similar.length>0 &&<CardsArray list={similar} title="Similar Movies"/>}
         </Container>
       </div>
 
